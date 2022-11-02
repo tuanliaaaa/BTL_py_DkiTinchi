c06385c720501e535b900d3bd2747d698780b7fd
tuanliaaaa <nhattuan44t.com>
2022-11-02 11:43:01 +0700

    deleteRoleAccount

--- Account/accountModels.py
+++ Account/accountModels.py
@@ -4,7 +4,6 @@ class Account(models.Model):
     username = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     status = models.IntegerField()
-    role = models.CharField(max_length=255)
     
     def __str__(self):
         return self.username
